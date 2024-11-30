e1e6f400e92588e21511ecb917b67b0bf9878169
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-30 21:06:46 +0200

    Feat(JS) Added a new property to an object

--- data.js
+++ data.js
@@ -3,46 +3,56 @@ const quotes = [
     quote:
       'Have the courage to follow your heart and intuition. They somehow already know what you truly want to become.',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote:
       'The greatest glory in living lies not in never falling, but in rising every time we fall.',
     author: 'Nelson Mandela',
+    isFavorite: false,
   },
   {
     quote: 'Life is what happens when you’re busy making other plans.',
     author: 'John Lennon',
+    isFavorite: false,
   },
   {
     quote:
       'The future belongs to those who believe in the beauty of their dreams.',
     author: 'Eleanor Roosevelt',
+    isFavorite: false,
   },
   {
     quote:
       'Success is not final, failure is not fatal: It is the courage to continue that counts.',
     author: 'Winston Churchill',
+    isFavorite: false,
   },
   {
     quote:
       'Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.',
     author: 'Buddha',
+    isFavorite: false,
   },
   {
     quote: 'In the middle of every difficulty lies opportunity.',
     author: 'Albert Einstein',
+    isFavorite: false,
   },
   {
     quote: 'You miss 100% of the shots you don’t take.',
     author: 'Wayne Gretzky',
+    isFavorite: false,
   },
   {
     quote: 'It does not matter how slowly you go as long as you do not stop.',
     author: 'Confucius',
+    isFavorite: false,
   },
   {
     quote: 'The only way to do great work is to love what you do.',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
 ];
 
